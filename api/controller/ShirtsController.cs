42b1580c87174cb7eedb19f94c78c0893c4d08e3
Tran Ha Khuong Duy <khuongduy.tech@gmail.com>
2024-10-07 16:18:21 +0700

    config: routing

---
+++ api/controller/ShirtsController.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.controller
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ShirtsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetShirts()
+        {
+            return "Reading all the shirts";
+        }
+        
+        [HttpGet ("{id}")]
+        public string GetShirtsById(int id)
+        {
+            return $"Reading shirt: {id} ";
+        }
+
+        [HttpPost]
+        public string CreateShirt()
+        {
+            return $"Create a Shirt";
+        }
+
+        [HttpPut ("{id}")]
+        public string UpdateShirtById(int id)
+        {
+            return $"Updating shirt: {id}";
+        }
+
+        [HttpDelete ("{id}")]
+        public string DeleteShirtById(int id)
+        {
+            return $"Deleting Shirt: {id}";
+        }
+    }
+}
